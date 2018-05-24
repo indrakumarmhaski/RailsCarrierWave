aa5ebe0ec1ced815b36d5b767dd229e59804f00e
indrakumarmhaski <indrakumarmhaski@gmail.com>
2018-05-24 10:48:09 +0530

    Completely implemented file uploading

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,13 +1,32 @@
 class ResumesController < ApplicationController
   def index
+    @resumes = Resume.all
   end
 
   def new
+    @resume = Resume.new
   end
 
   def create
+    @resume = Resume.new(resume_params)
+    if @resume.save
+      redirect_to resumes_path, notice: "The resume #{ @resume.name } has been saved"
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    @resume = Resume.find(params[:id])
+    @resume.destroy
+    redirect_to resumes_path, notice: "The resume #{ @resume.name } has been destroyed"
+
   end
+
+  private
+
+  def resume_params
+    params.require(:resume).permit(:name, :attachment)
+  end
+
 end
